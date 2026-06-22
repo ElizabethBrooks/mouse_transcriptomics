0d06d29e77fbd88ccb85e1f0b4a2c9e756b37589
ElizabethBrooks <merimaat1@gmail.com>
2026-06-22 19:51:47 -0400

    Updated stats script

--- 01_qc/stats_samtools.sh
+++ 01_qc/stats_samtools.sh
@@ -15,38 +15,23 @@
 # required modules for servers
 module load samtools
 
-# retrieve sorting method flags from input
-if [[ "$1" == "name" || "$1" == "Name" || "$1" == "n" || "$1" == "N" ]]; then
-	# name sorted flag with num threads flag
-	flags="-@ 4 -n"
-	methodTag="name"
-elif [[ "$1" == "coordinate" || "$1" == "Coordinate" || "$1" == "c" || "$1" == "C" ]]; then
-	# coordinate sorted with num threads flag
-	flags="-@ 4"
-	methodTag="coordinate"
-else
-	# report error with input flag
-	echo "ERROR: a flag for sorting method (name or coordinate) is expected... exiting"
-	exit 1
-fi
-
 # retrieve analysis outputs absolute path
 outputsPath=$(grep "outputs:" ../"inputData/inputPaths.txt" | tr -d " " | sed "s/outputs://g")
 
 # setup the inputs path
 inputsPath=$outputsPath"/aligned"
 
-# move to outputs directory
-cd "$outputsPath"
-
 # create outputs directory
-outputFolder="aligned_stats"
+outputFolder=$outputsPath"/aligned_stats"
 mkdir "$outputFolder"
 # check if the folder already exists
 if [ $? -ne 0 ]; then
 	echo "The $outputsPath directory already exsists... please remove before proceeding."
 	exit 1
 fi
+
+# move to outputs directory
+cd "$outputFolder"
 
 # name output file of inputs
 inputOutFile="$outputFolder"/"$outputFolder"_summary.txt
@@ -57,6 +42,11 @@ samtools --version > $inputOutFile
 for f1 in $inputsPath"/"*/; do
 	# name of aligned file
 	curAlignedSample="$f1"accepted_hits.bam
+	#Trim file path from current folder name
+	curSampleNoPath=$(basename "$f1")
 	# run samtolls stats
-	samtools stats -@ 8 $curAlignedSample
+	samtools stats -@ 8 $curAlignedSample > $outputFolder"/"$curSampleNoPath".stats"
 done
+
+# run multiqc to aggegrate the reports
+multiqc $outputFolder -o $outputFolder -n "multiqc"
